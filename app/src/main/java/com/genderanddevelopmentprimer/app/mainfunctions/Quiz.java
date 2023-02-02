9a488f39b056e20dbd0b03f61ab1482977c89fdc
Castor Troy <ocastortroy@gmail.com>
2023-02-02 20:17:51 +0800

    modified quiz activity and add app icon

--- app/src/main/java/com/genderanddevelopmentprimer/app/mainfunctions/Quiz.java
+++ app/src/main/java/com/genderanddevelopmentprimer/app/mainfunctions/Quiz.java
@@ -1,19 +1,21 @@
 package com.genderanddevelopmentprimer.app.mainfunctions;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.genderanddevelopmentprimer.app.R;
@@ -21,6 +23,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Objects;
 
@@ -36,6 +39,8 @@ public class Quiz extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         setBackground();
+
+
 
         fStore = FirebaseFirestore.getInstance();
 
@@ -55,7 +60,7 @@ public class Quiz extends AppCompatActivity {
 
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
-        linearLayout.setPadding(40,20,40,50);
+        linearLayout.setPadding(40, 20, 40, 50);
 
         DocumentReference docRef = fStore.collection("questions").document(val);
         docRef.get().addOnCompleteListener(task -> {
@@ -68,18 +73,16 @@ public class Quiz extends AppCompatActivity {
                         for (int i = 1; i <= Objects.requireNonNull(map).size(); i++) {
 
                             TextView tv = new TextView(Quiz.this);
-                            tv.setText(String.format("%d. %s", i, value.getString("Q" + i)));
+                            tv.setText(String.format("%d. %s", i, Objects.requireNonNull(value).getString("Q" + i)));
                             tv.setTextSize(17);
                             tv.setBackgroundColor(Color.parseColor("#95808080"));
-                            tv.setLayoutParams(new LinearLayout.LayoutParams(
-                                    LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            tv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
                             ans = new EditText(Quiz.this);
                             ans.setId(i);
                             ans.setHint("Answer");
                             ans.setInputType(InputType.TYPE_CLASS_TEXT);
-                            ans.setLayoutParams(new LinearLayout.LayoutParams(
-                                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                            ans.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
                             linearLayout.addView(tv);
                             linearLayout.addView(ans);
@@ -96,8 +99,13 @@ public class Quiz extends AppCompatActivity {
 
         //submit button
         btn.setOnClickListener(view -> {
-            DocumentReference documentReference1 = fStore.collection("answers").document(val);
 
+            ArrayList<String> answerList = new ArrayList<>();
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+            DocumentReference documentReference1 = fStore.collection("answers").document(val);
             //check then count the numbers inside the database
             documentReference1.get().addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
@@ -109,23 +117,34 @@ public class Quiz extends AppCompatActivity {
                         documentReference.addSnapshotListener(Quiz.this, (value, error) -> {
                             for (int i = 1; i <= Objects.requireNonNull(map).size(); i++) {
                                 EditText answers = findViewById(i);
-                                if (value != null) {
-                                    if (answers.getText().toString().equals(value.getString("ans"+i))){
-                                        Log.d("TAG", "onCreate: Correct");
-                                        score++;
-                                    }else{
-                                        Log.d("TAG", "onCreate: Mistake");
-                                    }
+
+                                if (answers.getText().toString().equals(Objects.requireNonNull(value).getString("ans" + i))) {
+                                    score++;
+                                    answerList.add(String.format("%d. %s ⁄", i, answers.getText().toString()));
+                                }else{
+                                    answerList.add(String.format("%d. %s ×", i, answers.getText().toString()));
                                 }
                             }
-                            Toast.makeText(Quiz.this, "Score: "+ score, Toast.LENGTH_LONG).show();
-                            score = 0;
+                            AlertDialog.Builder showScore = new AlertDialog.Builder(this);
+                            showScore.setTitle(String.format("Your score is: %d", score));
+                            showScore.setItems(answerList.toArray(new CharSequence[0]),null);
+                            showScore.setCancelable(false);
+                            showScore.setNegativeButton("Done", (dialog, which) -> finish());
+                            AlertDialog show = showScore.create();
+                            show.show();
+
+                            show.getButton(AlertDialog.BUTTON_NEGATIVE).setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+
+                            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT); //create a new one
+                            layoutParams.gravity = Gravity.END; //this is layout_gravity
+                            layoutParams.rightMargin = 10;
+
+                            show.getButton(AlertDialog.BUTTON_NEGATIVE).setLayoutParams(layoutParams);
                         });
                     }
                 }
             });
         });
-
         this.addContentView(sv, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
     }
 
